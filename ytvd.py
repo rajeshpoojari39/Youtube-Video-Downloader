192f7e351cf19d958fd22fc71fba849beb8372b8
rajeshpoojari39 <54307904+rajeshpoojari39@users.noreply.github.com>
2020-03-24 12:27:05 +0530

    Modified ytvd.py

--- ytvd.py
+++ ytvd.py
@@ -1,6 +1,6 @@
 from pytube import YouTube
 
-yt =YouTube("https://www.youtube.com/watch?v=Tqsz6fjvhZM")
+yt =YouTube("https://www.youtube.com/watch?v=2KsrtJ3VtZs")
 
 dw = yt.streams.get_highest_resolution()
 dw.download("C:/Users/acer/Desktop")
